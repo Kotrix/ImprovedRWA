7f2a0cbc007d58027dc51e24fdb47d22897be149
Krzysztof Kotowski <krzysztof.kotowski@polsl.pl>
2022-12-05 09:07:00 +0100

    Added a simple example

--- example.py
+++ example.py
@@ -12,11 +12,17 @@ epochs = mne.read_epochs_eeglab(file_with_epochs)
 evoked = epochs.average()
 evoked.plot(window_title="Traditional average")
 
-# RWA should be calculated only for the time window of interest
+rwa = RWA()
+
+# Simple example using the whole epoch for averaging
+evoked2 = epochs.average(method=rwa.run)
+evoked.plot(window_title="Improved RWA - whole epoch")
+
+# For more robust results RWA should be calculated only for the time window of interest
 sfreq = epochs.info['sfreq']
 tmin, tmax = -0.2, 0.9
 baseline = round(- tmin * sfreq)
 epoch_end = round(tmax * sfreq) + baseline
-evoked.data[..., baseline:epoch_end] = RWA().run(epochs.get_data()[..., baseline:epoch_end])
+evoked.data[..., baseline:epoch_end] = rwa.run(epochs.get_data()[..., baseline:epoch_end])
 
-evoked.plot(window_title="Improved RWA")
+evoked.plot(window_title="Improved RWA - time window of interest")
